479763c5b9efbd27e9bf9ad7fa3ddf2914f41703
duyht133 <duyhtfx13324@funix.edu.vn>
2022-01-04 00:19:51 +0700

    reactjs_lab03-2_asm1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import "./App.css";
+import React, { Component } from "react";
 import Menu from "./components/Menucomponent";
 import { Navbar, NavbarBrand } from "reactstrap";
+import {dishes} from "./shared/dishes";
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">tao moi reactjs</NavbarBrand>
-        </div>
-      </Navbar>
-   
-      <Menu/>
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-    </div>
-
-  );
+    this.state = {
+      dishes: dishes
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">tao moi reactjs</NavbarBrand>
+          </div>
+        </Navbar>
+     
+        <Menu dishes={this.state.dishes}/>
+  
+      </div>
+  
+    );
+  }
+  
 }
+  
+  
+
 
 export default App;
--- src/components/Menucomponent.js
+++ src/components/Menucomponent.js
@@ -1,77 +1,58 @@
 import React, { Component } from "react";
-import { Media } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
 
 class Menu extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      dishes:[
-        {
-          id: 0,
-          name:"pizzachicken",
-          image:"https://cdn.tgdd.vn/2020/09/CookProduct/1200bzhspm-1200x676.jpg",
-          category:"mains",
-          label:"Hot",
-          price:"4.99",
-          description:"ăn thơm mùi gà",
-        },
-        {
-          id: 2,
-          name:"pizzacow",
-          image:"https://img.dominos.vn/Veggie-mania.jpg",
-          category:"nuong",
-          label:"burn",
-          price:"8.99",
-          description:"pizza con bò cười",
-        },
-        {
-          id: 3,
-          name:"pizza8c",
-          image:"https://petercat.net/wp-content/uploads/2020/04/delish-homemade-pizza-horizontal-1542312378.png",
-          category:"hap",
-          label:"Hot",
-          price:"7.99",
-          description:"pizza hấp",
-        },
-        {
-          id: 4,
-          name:"pizzaquay",
-          image:"https://dulichkhampha24.com/wp-content/uploads/2020/09/pizza-ha-noi.jpg",
-          category:"quay",
-          label:"Hot",
-          price:"6.99",
-          description:"quay lên như bò",
-        },
-      ]
+      selectedDish: null
+    };
+  }
+
+  onDishSelect(e) {
+    this.setState({selectedDish:e});
+  }
+
+  renderDish(e) {
+    if(e != null) {
+      return(
+        <Card>
+          <CardImg width="100%" src={e.image} alt={e.name}></CardImg>
+          <CardBody>
+            <CardTitle>{e.name}</CardTitle>
+            <CardText>{e.description}</CardText>
+          </CardBody>
+        </Card>
+      )
+    }else{
+      return(
+        <div></div>
+      )
     }
   }
 
   render() {
-    const menu = this.state.dishes.map((e)=>{
+    const menu = this.props.dishes.map((e) => {
       return (
-          <div key={e.id} className="col-12 mt-5">
-            <Media tag="li">
-
-              <Media left middle>
-                  <Media object src={e.image} alt={e.name}/>
-              </Media>
-
-              <Media body className="ml-5">
-                <Media heading>{e.name}</Media>
-                <p>{e.description}</p>
-              </Media>
-
-            </Media>
-          </div>
-      )
+        <div key={e.id} className="col-12 col-md-5 m-1">
+          <Card onClick={()=>{this.onDishSelect(e)}}>
+            <CardImg width="100%" src={e.image} alt={e.name} />
+            <CardImgOverlay>
+              <CardTitle>{e.name}</CardTitle>
+              <p>{e.description}</p>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
     });
     return (
       <div className="container">
         <div className="row">
-          <Media list>
-              {menu}
-           </Media>
+          {menu}
+        </div>
+        <div className="row">
+          {this.renderDish(this.state.selectedDish)}
         </div>
       </div>
     );
